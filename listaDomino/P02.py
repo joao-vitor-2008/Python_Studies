39c18bca4e299ce2302820061058ae7f7901569d
João Vitor <joaovitornascimentolopes4@gmail.com>
2026-06-08 11:04:52 -0400

    Update P02.py
    Alterando para as mãos serem menores de 7

--- listaDomino/P02.py
+++ listaDomino/P02.py
@@ -4,7 +4,4 @@ def pert(x, a, b):
 
 def maop(mao):
     mao = [pedra for pedra in mao if pert(pedra[0], 0, 6) and pert(pedra[1], 0, 6)]
-    if len(mao) == 7:
-        return True
-    else:
-        return False
+     return pert(len(mao), 0, 7) and mao!=[]:
